101c638fbe95080ee9075ac50341f4c9cae1bc1e
Srinu <sssrinu2001@gmail.com>
2023-05-25 15:44:45 +0530

    'firstserve'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const data = localStorage.getItem('lists') ? JSON.parse(localStorage.getItem('lists')) : []
+  const [list, setList] = useState(data);
+  const [newTask, setNewTask] = useState('');
+  const [search, setSearch] = useState('');
+
+  const addTask = () => {
+    localStorage.setItem('lists',JSON.stringify([...list,newTask]))
+    setList([...list, newTask])
+    setNewTask("")
+  }
+  const deleteTask = (i) => {
+    const delList = [...list];
+    delList.splice(i,1);
+    setList(delList)
+    localStorage.setItem('lists', JSON.stringify(delList))
+  }
+  const updateTask = (e,i) => {
+    const uptask = [...list];
+    uptask.splice(i,1,e.target.value)
+    setList(uptask)
+    localStorage.setItem('lists', JSON.stringify(uptask))
+  }
+  const keyEnter = (e) => {
+    if(e.key === "Enter"){
+      addTask()
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='search'>
+        <input type='text' placeholder='Search task 🔎' onChange={(e) => {setSearch(e.target.value)}} />
+      </div>
+
+      <h1 className='heading'>To-Do App 🤺</h1>
+      <div className='inputs'>
+
+        <input type='text' onChange={(e) => { setNewTask(e.target.value) }} onKeyDown={keyEnter} value={newTask} />
+        <button className='btn' onClick={addTask}>Add Task 👍</button>
+
+      </div>
+
+      <div className='container'>
+        {
+          list.map((val,i) => {
+            if(val.toLowerCase().includes(search.toLowerCase())){
+              return (
+                <div className='list' key={i}>
+
+                  <input type='text' value={val} onChange={(e) => {updateTask(e,i)}} />
+                  <span className='icon' onClick={() => {deleteTask(i)}}>❌</span>
+
+                </div>
+              )
+            }else{
+              return ''
+            }
+          })
+        }
+      </div>
     </div>
   );
 }
